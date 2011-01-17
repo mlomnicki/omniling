12aa2b683b0873463744d618c911813114cf6537
Michał Łomnicki <michal.lomnicki@gmail.com>
2011-01-17 02:52:15 +0100

    updated en-US locale

--- chrome/content/lingpl.js
+++ chrome/content/lingpl.js
@@ -1,21 +1,23 @@
+// based on Zen Liu code
+// https://addons.mozilla.org/pl/firefox/addon/google-dictionary-and-google-t/
 var lingpl = {
-  self: this,
   onLoad: function() {
     // initialization code
     this.initialized = true;
     this.strings = document.getElementById("lingpl-strings");
     var appcontent = document.getElementById("appcontent");   // browser
+    lingpl.options.read();
     if (appcontent)
     {
       appcontent.addEventListener("load", this.contentLoadHandler, true);
     }
 
-    log("lingpl initialization complete");
+    lingpl.log("lingpl initialization complete");
   },
 
   contentLoadHandler : function(evt)
   {
-    log("this.contentLoadHandler starts");
+    lingpl.log("this.contentLoadHandler starts");
 
     var doc = evt.originalTarget;
     if (doc instanceof HTMLDocument)
@@ -23,88 +25,46 @@ var lingpl = {
       // register mouse handlers
       // we register them with content documents only because we
       // do not want the events fired when user click on menues, etc.
-      doc.addEventListener("dblclick", lingpl.clickHandler, true);
+      doc.addEventListener("click", lingpl.clickHandler, true);
       doc.addEventListener("mousemove", lingpl.mousemoveHandler, true);
     }
 
-    log("this.contentLoadHandler ends");
+    lingpl.log("this.contentLoadHandler ends");
   },
 
   clickHandler : function(evt)
   {
-    log("lingpl.clickHandler starts");
+    lingpl.log("lingpl.clickHandler starts");
 
     if (2 == evt.detail)
     {
-      if (lingpl.prefs.enableDoubleClick)
+      lingpl.log("enableDoubleClick: " + lingpl.options.enableDoubleClick);
+      if (lingpl.options.enableDoubleClick)
       {
         lingpl.select.lookup(evt);
       }
     }
     else if (1 == evt.detail)
     {
-      if (lingpl.prefs.enableSelect)
+      if (lingpl.options.enableSelect)
       {
         lingpl.select.lookup(evt);
       }
     }
 
-    log("lingpl.clickHandler ends");
+    lingpl.log("lingpl.clickHandler ends");
   },
 
   mousemoveHandler : function(evt)
   {
-    //log("lingpl.mousemoveHandler starts");
-
-    if (lingpl.prefs.enableCtrlHover)
+    if (lingpl.options.enableCtrlHover)
     {
       if (evt.ctrlKey && !evt.shiftKey && !evt.altKey && !evt.metaKey)
       {
         // Ctrl + hover
-        utils.hover.capture(evt);
+        lingpl.utils.hover.capture(evt);
       }
     }
-
-    //log("lingpl.mousemoveHandler ends");
-  },
-
-  prefs : {
-    read : function()
-    {
-      log("lingpl.prefs.read starts");
-
-      var prefs = Components.classes["@mozilla.org/preferences-service;1"].getService(Components.interfaces.nsIPrefService);
-      var myprefs = prefs.getBranch("extensions.lingpl.");
-
-      lingpl.prefs.setPreference("enableSelect", "bool");
-      lingpl.prefs.setPreference("enableDoubleClick", "bool");
-      lingpl.prefs.setPreference("enableCtrlHover", "bool");
-      lingpl.prefs.setPreference("queryUrl", "string");
-      lingpl.prefs.setPreference("responseSectionsNum", "int");
-
-      log("lingpl.prefs.read ends");
-    },
-
-    setPreference: function(key, type) {
-      var value = null;
-      switch(type) {
-      case 'bool': value = getBoolPref(key);
-      case 'int': value = getIntPref(key);
-      default: value = getStringPref(key);
-      }
-      log("Preference: " + key + " = " + value);
-      // if preference has a value assigned
-      if(value != null) {
-        lingpl.prefs[key] = value
-      }
-    },
-
-    // default values
-    enableSelect: false,
-    enableDoubleClick : true,
-    enableCtrlHover: true,
-    queryUrl: "http://www.ling.pl/php/lingfeed-30.php",
-    responseSectionsNum: 1
   },
 
   select : {
@@ -147,7 +107,7 @@ var lingpl = {
   dicInline : {
     translate: function(word, callback, mouseX, mouseY)
     {
-      log("lingpl.dicInline.translate starts");
+      lingpl.log("lingpl.dicInline.translate starts");
 
       lingpl.dicInline.word = null;
 
@@ -163,28 +123,31 @@ var lingpl = {
 
       }
 
-      log("lingpl.dicInline.translate ends");
+      lingpl.log("lingpl.dicInline.translate ends");
     },
 
     lookupDict: function()
     {
       var req = XMLHttpRequest();
       var params = "sType=2&word=" + lingpl.dicInline.word;
-      req.open("POST", lingpl.prefs.queryUrl, true);
+      req.open("POST", lingpl.options.queryUrl, true);
       req.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
       req.setRequestHeader("Content-length", params.length);
       req.setRequestHeader("Connection", "close");
       req.onreadystatechange = function () {
         if (req.readyState == 4)
         {
-          lingpl.dicInline.processDictResponse(req.responseText);
+          if(req.status == 200)
+            lingpl.dicInline.processDictResponse(req.responseText);
+          else
+            lingpl.popup.write(lingpl.strings.getString("service_error"));
         }
       };
       req.send(params);
     },
 
     processDictResponse: function(respText) {
-      log("Response: " + respText);
+      lingpl.log("Response: " + respText);
       lingpl.popup.write(respText);
     }
 
@@ -193,7 +156,7 @@ var lingpl = {
   popup : {
     show: function(mouseX, mouseY)
     {
-      lingpl.popup.write("Translating...");
+      lingpl.popup.write(lingpl.strings.getString("translating"));
       lingpl.popup.open(mouseX, mouseY + 18);
     },
 
@@ -211,54 +174,32 @@ var lingpl = {
       return document.getElementById("lingplpopup");
     },
 
-    HTMLParser: function(aHTMLString) {
-      var html = document.implementation.createDocument("http://www.w3.org/1999/xhtml", "html", null);
-      var body = document.createElementNS("http://www.w3.org/1999/xhtml", "body");
-      html.documentElement.appendChild(body);
-
-      body.appendChild(Components.classes["@mozilla.org/feed-unescapehtml;1"]
-        .getService(Components.interfaces.nsIScriptableUnescapeHTML)
-        .parseFragment(aHTMLString, false, null, body));
-
-      return body;
+    getContent: function() {
+      return document.getElementById("lingPlPopupContent");
     },
 
     write : function(text)
     {
-      log("lingpl.popup.write starts");
-      var doc = lingpl.popup.HTMLParser(text);
+      lingpl.log("lingpl.popup.write starts");
+      var doc = lingpl.utils.HTMLParser(text);
       var definitions = doc.getElementsByClassName("dictdef");
-      if (definitions) {
-        definitions = Array.slice(definitions, 0, lingpl.prefs.responseSectionsNum);
+      var html = doc;
+      if (definitions.length > 0) {
+        definitions = Array.slice(definitions, 0, lingpl.options.responseSectionsNum);
+        html = definitions
       }
-      var html = definitions || doc;
-      lingpl.popup.replace(html);
+      lingpl.utils.replaceHTML(lingpl.popup.getContent(), html);
 
-      log("lingpl.popup.write ends");
-    },
-
-    // can't use innerHTML property so here is workaround
-    replace: function(html) {
-      var container = document.getElementById("lingPlPopupContent");
-      var nodes = container.childNodes;
-      for(var i = 0; i < nodes.length; i++) {
-        container.removeChild(nodes[i]);
-      }
-      // when array we have to append nodes manually
-      if(html instanceof Array) {
-        for(var i = 0; i < html.length; i++)
-          container.appendChild(html[i]);
-      }
-      else {
-        container.appendChild(html);  
-      }
+      lingpl.log("lingpl.popup.write ends");
     },
 
     popup : null,
 
   },  // lingpl.popup
 
-  container: null
+  log: function(text) {
+    lingpl.utils.log(text);
+  }
 
 };
 
